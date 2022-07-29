05e035cbc74aaa1ea771ea524fa7bd2cd0cc11a4
Sarah Menu <sarah.menu@gmail.com>
2022-07-29 14:55:03 +0200

    tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,12 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+    @task = Task.create
+    redirect_to root_path, notice: 'New task added!'
+  end
+
 end
